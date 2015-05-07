50ef449dabcd44d549a65ff77cf6e7ab6ec43c38
Eros <eros.chen@fuhu.com>
2015-05-07 14:43:05 +0800

    first Commit

---
+++ src/com/movebox/application/AppApplication.java
@@ -0,0 +1,17 @@
+package com.movebox.application;
+
+import com.parse.Parse;
+
+import android.app.Application;
+
+public class AppApplication extends Application {
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		// Enable Local Datastore.
+		Parse.enableLocalDatastore(this);
+		Parse.initialize(this, "g8jP46eACcuqqExpuTUJglE4gMano1Sd2x6QPQSD", "iUda9EJLG1pKpl4xf1VGGIAKNmjU7IgmqsWNPEVk");
+
+	}
+}
